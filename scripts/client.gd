ac1a43e1a024e390275054f23bd8f50468c2eff2
orangepeas <jj2simmons@googlemail.com>
2026-07-19 21:06:56 +0100

    Update client.gd

--- scripts/client.gd
+++ scripts/client.gd
@@ -91,8 +91,8 @@ func add_player_steam(steam_id):
 	add_player(steam_id, multiplayer.get_remote_sender_id())
 	updateLobbyBoard()
 	for player in global.players:
-		#if global.players[player].lobbyHost == false:
-		send_updated_players.rpc_id(global.players[player].multiplayer_id, global.players)
+		if global.players[player].lobbyHost == false:
+			send_updated_players.rpc_id(global.players[player].multiplayer_id, global.players)
 	check_if_can_start_game()
 	print("host multiplayer.get_peers  after: ", multiplayer.get_peers())
 
